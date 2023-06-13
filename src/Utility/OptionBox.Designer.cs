0ea13bc69fc16e23f28ef777912f9f6e9e8e1702
Josh <gdwtaylor@gmail.com>
2023-06-13 15:11:45 -0400

    Created OptionBox
    OptionBox is a Form that displays an arbitrary number of buttons to the user. A call to AddOptions adds a row of buttons to the form corresponding to the string parameters. Rows are drawn centred in the form, currently.

---
+++ src/Utility/OptionBox.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace ParBoil.Utility;
+
+partial class OptionBox
+{
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+        if (disposing && (components != null))
+        {
+            components.Dispose();
+        }
+        base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+        Body = new Label();
+        SuspendLayout();
+        // 
+        // Body
+        // 
+        Body.AutoSize = true;
+        Body.Location = new Point(12, 9);
+        Body.Name = "Body";
+        Body.Size = new Size(69, 15);
+        Body.TabIndex = 0;
+        Body.Text = "Default Text";
+        // 
+        // OptionBox
+        // 
+        AutoScaleDimensions = new SizeF(7F, 15F);
+        AutoScaleMode = AutoScaleMode.Font;
+        ClientSize = new Size(284, 57);
+        Controls.Add(Body);
+        MaximizeBox = false;
+        MinimizeBox = false;
+        Name = "OptionBox";
+        ShowIcon = false;
+        SizeGripStyle = SizeGripStyle.Hide;
+        Text = "Options";
+        ResumeLayout(false);
+        PerformLayout();
+    }
+
+    #endregion
+
+    private Label Body;
+}
---
+++ src/Utility/OptionBox.cs
@@ -0,0 +1,83 @@
+﻿namespace ParBoil.Utility;
+
+public partial class OptionBox : Form
+{
+    public OptionBox(string bodyText = "")
+    {
+        InitializeComponent();
+
+        if (bodyText != "")
+        {
+            Body.Text = bodyText;
+            ClientSize = new Size(ClientSize.Width, Body.Height + 24);
+        }
+        else
+        {
+            Controls.Remove(Body);
+            ClientSize = new Size(ClientSize.Width, 12);
+        }
+    }
+
+    private string SelectedOption = "";
+    private List<Button> Options = new List<Button>();
+    private List<Panel> Panels = new List<Panel>();
+
+    internal void AddOptions(params string[] labels)
+    {
+        var panel = new Panel()
+        {
+            Height = 24,
+            Width = 0,
+        };
+
+        int maxWidth = Options.Count > 0 ? ClientSize.Width : 0;
+        int x = 12;
+
+        foreach (string label in labels)
+        {
+            var option = new Button();
+            option.Text = label;
+            option.Width = TextRenderer.MeasureText(label, Font).Width + 12;
+            option.Location = new Point(x, 0);
+            x += option.Width + 12;
+            panel.Width = x;
+            option.Click += delegate
+            {
+                SelectedOption = option.Text;
+                this.Close();
+            };
+            Options.Add(option);
+            panel.Controls.Add(option);
+        }
+
+        Controls.Add(panel);
+
+        if (maxWidth < panel.Width)
+        {
+            maxWidth = panel.Width;
+            panel.Location = new Point(0, ClientSize.Height);
+
+            foreach (var oldPanel in Panels)
+            {
+                oldPanel.Location = new Point((maxWidth - oldPanel.Width) / 2, oldPanel.Location.Y);
+            }
+        }
+
+        else
+        {
+            panel.Location = new Point((maxWidth - panel.Width) / 2, ClientSize.Height);
+        }
+
+        Panels.Add(panel);
+
+
+        ClientSize = new Size(maxWidth, ClientSize.Height + 35);
+    }
+
+    internal string ShowOptions()
+    {
+        ShowDialog();
+
+        return SelectedOption;
+    }
+}
